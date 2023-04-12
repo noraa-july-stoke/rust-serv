62064b9cc3acfaa9c1b4ab30707e09fddf0357f3
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-04-12 00:25:30 -0400

    branch init 🪴

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 // use std::net::TcpStream;
 
-fn main () {
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server listening on port 8080");
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
 
-        let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
